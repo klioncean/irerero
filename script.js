3ffe9326802a3dfeff73426df95dba2c48b4e7b6
klioncean@gmail.co <klioncean@gmail.com>
2024-04-10 00:53:32 +0200

    new update

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Get all the content sections
+ 
   var sections = document.querySelectorAll('.content-section');
 
-  // Add click event listeners to navigation links
   var navLinks = document.querySelectorAll('nav a');
   navLinks.forEach(function(link) {
     link.addEventListener('click', function(event) {
       event.preventDefault();
       var targetId = link.getAttribute('href').substring(1);
-      // Scroll to the selected section
-      document.getElementById(targetId).scrollIntoView({behavior: "smooth"});
+          document.getElementById(targetId).scrollIntoView({behavior: "smooth"});
     });
   });
+  
 });
 
 document.addEventListener("DOMContentLoaded", function() {
