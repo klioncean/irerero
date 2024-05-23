426be310591159f379726aa0203d8de2c2709282
klioncean@gmail.co <klioncean@gmail.com>
2024-05-23 13:57:25 +0200

    new

--- languagemode.js
+++ languagemode.js
@@ -15,9 +15,28 @@ const translations = {
       subcourseMeaningFour: "Unlock the power of data analysis and visualization.",
       subcourseMeaningFive: "Grow your business with expert digital marketing strategies.",
       startLearning: "Start Learning",
+      Overviewtitle: "Overview",
+      missiontitle: "iR's Mission",
       overview: "Irerero is an online platform dedicated to providing high-quality digital skills and learning experiences to users worldwide. Since our inception in 2023, we have been committed to making digital skills and learning accessible, effective, and enjoyable for learners of all ages and backgrounds.",
       mission: "Our mission at Irerero is to empower individuals to achieve their digital skills learning goals and unlock new opportunities in their personal and professional lives. We believe that digital skills are valuable assets that open doors to freedom, global communication, and personal growth.",
       abouttrnslt: "Irerero is an online platform dedicated to providing high-quality digital skills and learning experiences to users worldwide. Since our inception in 2023, we have been committed to making digital skills and learning accessible, effective, and enjoyable for learners of all ages and backgrounds.",
+      missionCardHeader: "Global Reach",
+      missionCardHeaderTwo: "Expert Instructors",
+      missionCardHeaderThree: "Unlock Opportunities",
+      missionCardHeaderFour: "Personal Growth",
+      missionCardinfo: "We aim to provide access to digital education for learners worldwide.",
+      missionCardinfoTwo: "Learn from industry experts who are passionate about teaching.",
+      missionCardinfoThree: "Gain skills that open doors to new personal and professional opportunities.",
+      missionCardinfoFour: "Our courses are designed to help you grow and achieve your goals.",
+      whyus: "Why Choose Us",
+      whyusTwo: "High Satisfaction",
+      whyusThree: "Certified Courses",
+      whyusInfo: "Discover the key benefits of choosing our platform for your digital learning journey. We offer unique advantages that set us apart and ensure your success.",
+      whyusInfoTwo: "Join thousands of satisfied learners who have transformed their lives.",
+      whyusInfoThree: "Earn recognized certificates to showcase your skills and knowledge.",
+      contactus: "Contact Us",
+      contactusInfo: "If you have any questions, feedback, or inquiries, please feel free to reach out to us:",
+
     },
     fr: {
       home: "Accueil",
@@ -35,9 +54,27 @@ const translations = {
       subcourseMeaningFour: "Libérez la puissance de l'analyse et de la visualisation des données.",
       subcourseMeaningFive: "Développez votre entreprise avec des stratégies de marketing numérique expertes.",
       startLearning: "Commencer à apprendre",
+      Overviewtitle: "Aperçu",
+      missiontitle: "La mission d'IR",
       overview: "Irerero est une plateforme en ligne dédiée à fournir des compétences numériques et des expériences d'apprentissage de haute qualité aux utilisateurs du monde entier. Depuis notre création en 2023, nous nous engageons à rendre les compétences et l'apprentissage numériques accessibles, efficaces et agréables pour les apprenants de tous âges et de tous horizons.",
       mission: "Notre mission chez Irerero est de permettre aux individus d'atteindre leurs objectifs d'apprentissage des compétences numériques et de débloquer de nouvelles opportunités dans leur vie personnelle et professionnelle. Nous pensons que les compétences numériques sont des atouts précieux qui ouvrent les portes de la liberté, de la communication mondiale et de la croissance personnelle.",
       abouttrnslt: "Irerero est une plateforme en ligne dédiée à fournir des compétences numériques et des expériences d'apprentissage de haute qualité aux utilisateurs du monde entier. Depuis notre création en 2023, nous nous engageons à rendre les compétences et l'apprentissage numériques accessibles, efficaces et agréables pour les apprenants de tous âges et de tous horizons.",
+      missionCardHeader: "Portée mondiale",
+      missionCardHeaderTwo: "Instructeurs experts",
+      missionCardHeaderThree: "Débloquez des opportunités",
+      missionCardHeaderFour: "Croissance personnelle",
+      missionCardinfo: "Notre objectif est de fournir un accès à l’éducation numérique aux apprenants du monde entier.",
+      missionCardinfoTwo: "Apprenez auprès d’experts de l’industrie passionnés par l’enseignement.",
+      missionCardinfoThree: "Acquérir des compétences qui ouvrent les portes à de nouvelles opportunités personnelles et professionnelles.",
+      missionCardinfoFour: "Nos cours sont conçus pour vous aider à grandir et à atteindre vos objectifs.",
+      whyus: "Pourquoi nous choisir",
+      whyusTwo: "Satisfaction élevée",
+      whyusThree: "Cours certifiés",
+      whyusInfo: "Découvrez les principaux avantages de choisir notre plateforme pour votre parcours d'apprentissage numérique. Nous offrons des avantages uniques qui nous distinguent et assurent votre succès.",
+      whyusInfoTwo: "Rejoignez des milliers d'apprenants satisfaits qui ont transformé leur vie.",
+      whyusInfoThree: "Obtenez des certificats reconnus pour mettre en valeur vos compétences et vos connaissances.",
+      contactus: "Contactez-nous",
+      contactusInfo: "Si vous avez des questions, des commentaires ou des demandes de renseignements, n'hésitez pas à nous contacter:",
     },
     rw: {
       home: "Ahabanza",
@@ -55,9 +92,27 @@ const translations = {
       subcourseMeaningFour: "Fungura imbaraga zo gusesengura amakuru no kubonerana.",
       subcourseMeaningFive: "Kuza ubucuruzi bwawe hamwe nubuhanga bwo kwamamaza bwa digitale.",
       startLearning: "Tangira Kwiga",
+      Overviewtitle: "Incamake",
+      missiontitle: "Inshingano za iR",
       overview: "Irerero ni urubuga rwa interineti rwagenewe gutanga ubumenyi buhanitse bwa digitale hamwe nuburambe bwo kwiga kubakoresha kwisi yose. Kuva twashingwa mu 2023, twiyemeje gukora ubumenyi bwa digitale no kwiga bigerwaho, bikora neza, kandi bishimishije kubiga imyaka yose kandi bakomoka.",
       mission: "Inshingano yacu muri Irerero ni uguha imbaraga abantu kugirango bagere ku ntego zabo zo kwiga ubumenyi bwa digitale no gufungura amahirwe mashya mubuzima bwabo bwite kandi bwumwuga. Twizera ko ubumenyi bwa digitale ari umutungo w'agaciro ufungura imiryango y'ubwisanzure, itumanaho ku isi, no kuzamuka kwa muntu.",
       abouttrnslt: "Irerero ni urubuga rwa interineti rwagenewe gutanga ubumenyi buhanitse bwa digitale hamwe nuburambe bwo kwiga kubakoresha kwisi yose. Kuva twashingwa mu 2023, twiyemeje gukora ubumenyi bwa digitale no kwiga bigerwaho, bikora neza, kandi bishimishije kubiga imyaka yose kandi bakomoka.",
+      missionCardHeader: "Kugera ku Isi",
+      missionCardHeaderTwo: "Abigisha b'inzobere",
+      missionCardHeaderThree: "Fungura amahirwe",
+      missionCardHeaderFour: "Gukura kwawe",
+      missionCardinfo: "Intego yacu ni ugutanga uburyo bwo kwigisha hifashishijwe abiga kwisi yose.",
+      missionCardinfoTwo: "Wigire kubuhanga bwinganda bashishikajwe no kwigisha.",
+      missionCardinfoThree: "Shaka ubuhanga bukingura amarembo mashya yumuntu ku giti cye nu mwuga.",
+      missionCardinfoFour: "Amasomo yacu yagenewe kugufasha gukura no kugera kuntego zawe.",
+      whyus: "Kuki uduhitamo",
+      whyusTwo: "Kwishimira cyane",
+      whyusThree: "Amasomo yemewe",
+      whyusInfo: "Menya inyungu zingenzi zo guhitamo urubuga rwurugendo rwawe rwo kwiga. Dutanga ibyiza byihariye bidutandukanya kandi byemeza intsinzi yawe.",
+      whyusInfoTwo: "Injira ibihumbi byabiga banyuzwe bahinduye ubuzima bwabo.",
+      whyusInfoThree: "Shaka ibyemezo byemewe kugirango werekane ubuhanga n'ubumenyi.",
+      contactus: "Twandikire",
+      contactusInfo: "Niba ufite ibibazo, ibitekerezo, cyangwa ibibazo, nyamuneka utugereho:"
     }
   };
 
@@ -82,9 +137,27 @@ const translations = {
     document.querySelector('.btntlnslt4').textContent = translations[language].startLearning;
     document.querySelector('.btntlnslt5').textContent = translations[language].startLearning;
     document.querySelector('.information').textContent = translations[language].information;
+    document.querySelector('.Overviewtitle').textContent = translations[language].Overviewtitle;
+    document.querySelector('.missiontitle').textContent = translations[language].missiontitle;
     document.querySelector('.overview').textContent = translations[language].overview;
     document.querySelector('.missiontrnslt').textContent = translations[language].mission;
     document.querySelector('.abouttrnslt').textContent = translations[language].abouttrnslt;
+    document.querySelector('.missionCardHeader').textContent = translations[language].missionCardHeader;
+    document.querySelector('.missionCardHeaderTwo').textContent = translations[language].missionCardHeaderTwo;
+    document.querySelector('.missionCardHeaderThree').textContent = translations[language].missionCardHeaderThree;
+    document.querySelector('.missionCardHeaderFour').textContent = translations[language].missionCardHeaderFour;
+    document.querySelector('.missionCardinfo').textContent = translations[language].missionCardinfo;
+    document.querySelector('.missionCardinfoTwo').textContent = translations[language].missionCardinfoTwo;
+    document.querySelector('.missionCardinfoThree').textContent = translations[language].missionCardinfoThree;
+    document.querySelector('.missionCardinfoFour').textContent = translations[language].missionCardinfoFour;
+    document.querySelector('.whyus').textContent = translations[language].whyus;
+    document.querySelector('.whyusTwo').textContent = translations[language].whyusTwo;
+    document.querySelector('.whyusThree').textContent = translations[language].whyusThree;
+    document.querySelector('.whyusInfo').textContent = translations[language].whyusInfo;
+    document.querySelector('.whyusInfoTwo').textContent = translations[language].whyusInfoTwo;
+    document.querySelector('.whyusInfoThree').textContent = translations[language].whyusInfoThree;
+    document.querySelector('.contactus').textContent = translations[language].contactus;
+    document.querySelector('.contactusInfo').textContent = translations[language].contactusInfo;
     localStorage.setItem('selectedLanguage', language);
   }
 
